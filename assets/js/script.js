c662510b0df6f7cac4bbc1703ea7ae358666b65a
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2022-12-18 21:40:43 +1100

    html and css update, questions in js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+/*
+Questions
+
+What does javaScript provide to code?", ["a. Style", "b. Interactive Content", "c. Website Foundation", "d. None of the above"], "b. Interactive Content"
+The following code is an example of what? for (var i=0, i < userlength, i++)", ["a. Function", "b. For Loop", "c. Object", "d. Array"], "b. For Loop"
+How many items are in an array with an index of 5?", ["a. 4", "b. 5", "c. 6", "d. 7"], "d. 6"
+What is a function call?", ["a. A request made by a program or script that performs a predetermined function.", "b. A special variable, which can hold more than one value at a time.", "c. A method.", "d. None of the Above"], "a. A request made by a program or script that performs a predetermined function."
+How do you call the function showScore??", ["a. showScore()", "b. Undefined", "c. function ()", "d. function showScore(score)"], "a. showScore()"
+*/
